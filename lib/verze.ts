3daea0edd01cbfbf283bd3d72a92f1f0c82372f4
viktorialashes <matuchovic@gmail.com>
2026-08-27 19:22:59 +0200

    novy znak a podklad pod znackou

--- lib/verze.ts
+++ lib/verze.ts
@@ -9,10 +9,10 @@
  * nedá zapomenout. Původní řešení záviselo na tom, že si někdo
  * vzpomene zvednout konstantu, a dvakrát za sebou se to nestalo.
  */
-export const VERZE = "1.34.2";
+export const VERZE = "1.34.3";
 
 /** Jedna věta o tom, co se změnilo. „Nová verze" nikoho nepřesvědčí. */
-export const VERZE_POPIS = "Značka se vejde do panelu celá.";
+export const VERZE_POPIS = "Nový znak a podklad pod značkou.";
 
 /**
  * Důležitá oprava se nedá odložit.
